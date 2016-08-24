a810ca5ee55fc9c280715cc5bf61a9ae8d9a66c9
selfresonator <selfresonator@gmail.com>
2016-08-23 22:18:45 -0700

    Successfully created a School 'class', and instantiated it with a name of "Berklee"

--- myFirstJavaProgram.java
+++ myFirstJavaProgram.java
@@ -10,7 +10,7 @@ class School {
     return schoolName;
   }
 
-  public static void main() {
+  public static void main(String[] args) {
     School college = new School("Berklee");
     String sName = college.getName();
     System.out.println(sName);
