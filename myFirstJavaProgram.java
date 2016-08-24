69b9834530c579581ec867590c4fcf5cd2fd0029
selfresonator <selfresonator@gmail.com>
2016-08-23 21:58:06 -0700

    Created a class for schools.

--- myFirstJavaProgram.java
+++ myFirstJavaProgram.java
@@ -1,5 +1,14 @@
 import java.util.HashMap;
 
 public class School() {
-  
+
+  public School(String name) {
+    this.name = name;
+  }
+
+  public static void main() {
+    School college = new School("Berklee");
+    System.out.println(college);
+  }
+
 }
